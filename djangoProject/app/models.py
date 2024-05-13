444054d6908056395552a8e422048ff40c4fe778
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-05-13 17:03:49 +0700

    Table models in the database have been created

--- djangoProject/app/models.py
+++ djangoProject/app/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserImage(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    image = models.ImageField(verbose_name="Изображение")
+
+    class Meta:
+        db_table = 'user_image'
+        verbose_name = "Изображение пользователя"
+        verbose_name_plural = "Изображении пользователей"
+
+    def __str__(self):
+        return str(self.user.name)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=150, blank=False, null=False, verbose_name="Название посещенного места" )
+    description = models.CharField(max_length=400, blank=False, null=False, verbose_name="Описание посещенного места")
+    longitude = models.FloatField(null=False, blank=False, verbose_name="Широта посещенного места")
+    latitude = models.FloatField(null=False, blank=False, verbose_name="Долгота посещенного места")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
