aba0af40184e60bc8208e67e29a668f624bcc6b1
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-05-18 20:06:02 +0700

    Home page implemented

--- djangoProject/app/views.py
+++ djangoProject/app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .models import Place
 def index(request):
     return render(request, 'index.html')
+
+@login_required
 def main_page(request):
-    return render(request, 'main.html')
+    user_places = Place.objects.filter(user=request.user)
+    context = {'user_places': user_places}
+    return render(request, 'main.html', context)
